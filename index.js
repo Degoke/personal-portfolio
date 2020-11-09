5a54780a85ef57d8d16408df3ed02b4ef9ea170f
Degoke <69061153+Degoke@users.noreply.github.com>
2020-11-09 19:05:05 +0100

    mobile edit

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-$(".form").on("submit", function (event) {
-  event.preventDefault(); // prevent reload
-
-  var formData = new FormData(this);
-  formData.append("service_id", "service_ibq7bhi");
-  formData.append("template_id", "template_likkl0t");
-  formData.append("user_id", "user_YnxNO7mW1obMdVQjSXTGD");
-
-  $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
-    type: "POST",
-    data: formData,
-    contentType: false,
-    processData: false,
-  })
-    .done(function () {
-      swal(
-        "Your mail is sent!",
-        "You'll get a reply as soon as possible",
-        "success"
-      );
+$(function () {
+  $(".form").on("submit", function (event) {
+    event.preventDefault(); // prevent reload
+  
+    var formData = new FormData(this);
+    formData.append("service_id", "service_ibq7bhi");
+    formData.append("template_id", "template_likkl0t");
+    formData.append("user_id", "user_YnxNO7mW1obMdVQjSXTGD");
+  
+    $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
+      type: "POST",
+      data: formData,
+      contentType: false,
+      processData: false,
     })
-    .fail(function (error) {
-      swal("Oops...!", "could not send mail", "error", {
-        button: "Try again",
+      .done(function () {
+        swal(
+          "Your mail is sent!",
+          "You'll get a reply as soon as possible",
+          "success"
+        );
+      })
+      .fail(function (error) {
+        swal("Oops...!", "could not send mail", "error", {
+          button: "Try again",
+        });
       });
-    });
-});
+  });
+})
+
 
 function myFunction(x) {
   x.classList.toggle("change");
